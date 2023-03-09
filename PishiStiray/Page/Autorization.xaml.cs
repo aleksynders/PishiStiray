9a301709b2137a3623edf8206346830d2071f10d
user228 <forfarmcard@mail.ru>
2023-03-09 12:29:38 +0300

    up

--- PishiStiray/Page/Autorization.xaml.cs
+++ PishiStiray/Page/Autorization.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace PishiStiray
 {
@@ -20,9 +22,63 @@ namespace PishiStiray
     /// </summary>
     public partial class Autorization : Page
     {
+
+        int countError;
+        public static bool correct;
+        int countTime;
+        DispatcherTimer disTimer = new DispatcherTimer();
+
         public Autorization()
         {
             InitializeComponent();
+            countError = 0;
+            correct = false;
+            disTimer.Interval = new TimeSpan(0, 0, 1);
+            disTimer.Tick += new EventHandler(DisTimer_Tick);
+        }
+
+        private void DisTimer_Tick(object sender, EventArgs e)
+        {
+            if (countTime == 0)
+            {
+                AutBtn.IsEnabled = true;
+                GuestBtn.IsEnabled = true;
+                disTimer.Stop();
+                tbCode.Visibility = Visibility.Collapsed;
+            }
+            else
+                tbCode.Text = "Получить новый код можно будет через " + countTime + " секунд";
+            countTime--;
+        }
+
+        private void AutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            User user = BaseClass.BD.User.FirstOrDefault(x => x.UserLogin == tbLogin.Text && x.UserPassword == pbPassword.Password);
+            if (user != null)
+                FrameClass.frame.Navigate(new ListProduct(user));
+            else
+            {
+                if (countError == 0)
+                {
+                    MessageBox.Show("Пользователь с таким логиным и паролем не найден");
+                    countError++;
+                }
+                else
+                {
+                    CAPTCHA captcha = new CAPTCHA();
+                    captcha.ShowDialog();
+                    countError++;
+                    if (!correct)
+                    {
+                        AutBtn.IsEnabled = false;
+                        GuestBtn.IsEnabled = false;
+                        countTime = 10;
+                        tbCode.Text = "Получить новый код можно будет через " + countTime + " секунд";
+                        tbCode.Visibility = Visibility.Visible;
+                        disTimer.Start();
+                    }
+                }
+            }
         }
     }
 }
---
+++ PishiStiray/Window/CAPTCHA.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PishiStiray
+{
+    /// <summary>
+    /// Логика взаимодействия для CAPTCHA.xaml
+    /// </summary>
+    public partial class CAPTCHA : Window
+    {
+        public string text;
+        public CAPTCHA()
+        {
+            InitializeComponent();
+            Random rand = new Random();
+            int kolText = 4;
+            text = "";
+            for (int i = 0; i < kolText; i++)
+            {
+                int j = rand.Next(2);
+                if (j == 0)
+                    text = text + rand.Next(9).ToString();
+                else
+                {
+                    int l = rand.Next(2);
+                    if (l == 0)
+                    {
+                        text = text + (char)rand.Next('A', 'Z' + 1);
+                    }
+                    else
+                    {
+                        text = text + (char)rand.Next('a', 'z' + 1);
+                    }
+                }
+            }
+            int widthBegin = 0;
+            int widthEnd = 0;
+            int h = (int)CvField.Width / text.Length;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i == 0) 
+                    widthEnd += h;
+                else
+                {
+                    widthBegin = widthEnd;
+                    widthEnd += h;
+                }
+                int height = rand.Next((int)CvField.Height);
+                int width = rand.Next(widthBegin, widthEnd);
+                if (height > 170)
+                    height -= 30;
+                if (width > 590)
+                    widthEnd -= 10;
+                int fontSize = rand.Next(16, 33);
+                TextBlock txt = new TextBlock()
+                {
+                    Text = text[i].ToString(),
+                    TextDecorations = TextDecorations.Strikethrough,
+                    Padding = new Thickness(width, height, 0, 0),
+                    FontSize = fontSize,
+                };
+                CvField.Children.Add(txt);
+            }
+            int kolLine = rand.Next(5, 16);
+            for (int i = 0; i < kolLine; i++)
+            {
+                SolidColorBrush brush = new SolidColorBrush(Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256)));
+                Line l = new Line()
+                {
+                    X1 = rand.Next((int)CvField.Width),
+                    Y1 = rand.Next((int)CvField.Height),
+                    X2 = rand.Next((int)CvField.Width),
+                    Y2 = rand.Next((int)CvField.Height),
+                    Stroke = brush,
+                };
+                CvField.Children.Add(l);
+            }
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbInputField.Text == text)
+            {
+                Autorization.correct = true;
+                this.Close();
+            }
+            else
+            {
+                Autorization.correct = false;
+                this.Close();
+            }
+        }
+    }
+}
